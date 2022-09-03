6cfb1f0d384fbdfd722a79f4ea419bcc999b6f87
chapman-mg <chapman.mitch@gmail.com>
2022-09-03 15:54:15 +0800

    Add Saved Patches feature

--- patches.js
+++ patches.js
@@ -1,274 +1,279 @@
 //detect page height
 const documentHeight = () => {
-    const doc = document.documentElement;
-    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
-   }
-   window.addEventListener('resize', documentHeight);
-   documentHeight();
+  const doc = document.documentElement;
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', documentHeight);
+documentHeight();
 
 let artist = [
-    'Tangerine Dream',
-    'Kraftwerk',
-    'Vangelis',
+  'Tangerine Dream',
+  'Kraftwerk',
+  'Vangelis',
 
 ]
 let instrument = [
-    '303',
-    'violin',
-    'choir',
-    'vocoder',
-    'blues',
-    'horns',
-    'swell',
-    'vox',
-    'voices',
-    'strings',
-    'octaves',
-    'fifth',
-    'orchestra',
-    'pad',
-    'pulse',
-    'saw',
-    'space',
-    'piano',
-    'harpsichord',
-    'clav',
-    'bell',
-    'organ',
-    'guitar',
-    'brass',
-    'soundtrack',
-    'drums',
-    'noise',
-    'sitar',
-    'horn',
-    'split',
-    'stack',
-    'lead',
-    'rhythm',
-    'arpeggiator',
-    'transients',
-    'layers',
-    'sync',
-    'vowels',
-    'rhodes',
-    'e-piano',
-    'wurli',
-    'flute',
-    'glide',
-    'legato',
-    'dreams',
-    'shakuhachi',
-    'panpipes',
-    'square',
-    'birds',
-    'percussion',
-    'marimba',
+  '303',
+  'violin',
+  'choir',
+  'vocoder',
+  'blues',
+  'horns',
+  'swell',
+  'vox',
+  'voices',
+  'strings',
+  'octaves',
+  'fifth',
+  'orchestra',
+  'pad',
+  'pulse',
+  'saw',
+  'space',
+  'piano',
+  'harpsichord',
+  'clav',
+  'bell',
+  'organ',
+  'guitar',
+  'brass',
+  'soundtrack',
+  'drums',
+  'noise',
+  'sitar',
+  'horn',
+  'split',
+  'stack',
+  'lead',
+  'rhythm',
+  'arpeggiator',
+  'transients',
+  'layers',
+  'sync',
+  'vowels',
+  'rhodes',
+  'e-piano',
+  'wurli',
+  'flute',
+  'glide',
+  'legato',
+  'dreams',
+  'shakuhachi',
+  'panpipes',
+  'square',
+  'birds',
+  'percussion',
+  'marimba',
 
 ]
 
 let suffix = [
-    'solo',
-    'layers',
-    'sweep',
-    'stack',
-    'split',
-    'drone',
-    'bass',
-    'pad',
-    'lead',
-    'delay',
-    'blips',
-    'sequence',
-    'bleeps',
-    'bloops',
-    'stab',
-    
+  'solo',
+  'layers',
+  'sweep',
+  'stack',
+  'split',
+  'drone',
+  'bass',
+  'pad',
+  'lead',
+  'delay',
+  'blips',
+  'sequence',
+  'bleeps',
+  'bloops',
+  'stab',
+
 ]
 
 let genre = [
-    'filtered',
-    'Berlin School',
-    'atmospheric',
-    'acid',
-    'techno',
-    'alien',
-    'industrial',
-    'house',
-    'trance',
-    'psy',
-    'drone',
-    'ambient',
-    'synthwave',
-    'glitch',
-    'electro',
-    'folk',
-    'christmas',
-    'Kraftwerk',
-    'Vangelis',
-    'EDM',
-    'S&H',
-    'Carpenter',
-    'tron',
-    'funk',
-    'Moroder',
-    'tube',
-    'rave',
-    'boom-bap',
-    'LFO',
+  'filtered',
+  'Berlin School',
+  'atmospheric',
+  'acid',
+  'techno',
+  'alien',
+  'industrial',
+  'house',
+  'trance',
+  'psy',
+  'drone',
+  'ambient',
+  'synthwave',
+  'glitch',
+  'electro',
+  'folk',
+  'christmas',
+  'Kraftwerk',
+  'Vangelis',
+  'EDM',
+  'S&H',
+  'Carpenter',
+  'tron',
+  'funk',
+  'Moroder',
+  'tube',
+  'rave',
+  'boom-bap',
+  'LFO',
 
 ]
 let adjective = [
-    'PCM',
-    'ice',
-    'crystal',
-    'PWM',
-    'slap',
-    'psycho',
-    'resonant',
-    'modulated',
-    'ringmod',
-    'distorted',
-    'ambient',
-    'aggro',
-    'modern',
-    'sad',
-    'big',
-    'chorused',
-    'interstellar',
-    'cheesy',
-    'haunting',
-    'mellow',
-    'thick',
-    'reedy',
-    'bouncy',
-    'delayed',
-    'CS-80',
-    'oberheim',
-    'prophet',
-    'modular',
-    'moog',
-    'roland',
-    'juno',
-    'jupiter',
-    'fat',
-    'soaring',
-    'tearing',
-    'ripping',
-    '80s',
-    '70s',
-    '90s',
-    'analog',
-    'digital',
-    'wavetable',
-    'granular',
-    'FM',
-    'sync',
-    'metallic',
-    'chaotic',
-    'bowed',
-    'plucked',
-    'electric',
-    'staccato',
-    'muted',
-    'long',
-    'short',
-    'detuned',
-    'clean',
-    'glitchy',
-    'mysterious',
-    'forlorn',
-    'ethereal',
-    'noise',
-    'jet',
-    '101',
-    'moog',
-    'power',
-    'atmosphere',
-    'majestic',
-    'vintage',
-    'slow',
-    'brass',
-    'phat',
-    'warm',
-    'fuzzy',
-    'rezo',
-    'hybrid',
-    'dark',
-    'vox',
-    'glass',
-    'velo',
-    'vocal',
-    'synth',
-    'bright',
-    'deep',
-    'sonic',
-    'phased',
-    'pulse',
-    'pop',
-    'glassy',
-    'shiny',
-    'chime',
-    'pure',
-    'tortured',
-    'poly',
-    'alternative',
-    'rocker',
-    'jazz',
-    'crunch',
-    'running',
-    'flying',
-    'steel',
-    'groovy',
+  'PCM',
+  'ice',
+  'crystal',
+  'PWM',
+  'slap',
+  'psycho',
+  'resonant',
+  'modulated',
+  'ringmod',
+  'distorted',
+  'ambient',
+  'aggro',
+  'modern',
+  'sad',
+  'big',
+  'chorused',
+  'interstellar',
+  'cheesy',
+  'haunting',
+  'mellow',
+  'thick',
+  'reedy',
+  'bouncy',
+  'delayed',
+  'CS-80',
+  'oberheim',
+  'prophet',
+  'modular',
+  'moog',
+  'roland',
+  'juno',
+  'jupiter',
+  'fat',
+  'soaring',
+  'tearing',
+  'ripping',
+  '80s',
+  '70s',
+  '90s',
+  'analog',
+  'digital',
+  'wavetable',
+  'granular',
+  'FM',
+  'sync',
+  'metallic',
+  'chaotic',
+  'bowed',
+  'plucked',
+  'electric',
+  'staccato',
+  'muted',
+  'long',
+  'short',
+  'detuned',
+  'clean',
+  'glitchy',
+  'mysterious',
+  'forlorn',
+  'ethereal',
+  'noise',
+  'jet',
+  '101',
+  'moog',
+  'power',
+  'atmosphere',
+  'majestic',
+  'vintage',
+  'slow',
+  'brass',
+  'phat',
+  'warm',
+  'fuzzy',
+  'rezo',
+  'hybrid',
+  'dark',
+  'vox',
+  'glass',
+  'velo',
+  'vocal',
+  'synth',
+  'bright',
+  'deep',
+  'sonic',
+  'phased',
+  'pulse',
+  'pop',
+  'glassy',
+  'shiny',
+  'chime',
+  'pure',
+  'tortured',
+  'poly',
+  'alternative',
+  'rocker',
+  'jazz',
+  'crunch',
+  'running',
+  'flying',
+  'steel',
+  'groovy',
 
 
 ]
 
 //generate random patch name
 function randomPatch() {
-    firstPart = adjective[Math.floor(Math.random() * adjective.length)];
-    secondPart = instrument[Math.floor(Math.random() * instrument.length)];
-    suffixPart = suffix[Math.floor(Math.random() * suffix.length)];
-    genrePart = genre[Math.floor(Math.random() * genre.length)];
+  firstPart = adjective[Math.floor(Math.random() * adjective.length)];
+  secondPart = instrument[Math.floor(Math.random() * instrument.length)];
+  suffixPart = suffix[Math.floor(Math.random() * suffix.length)];
+  genrePart = genre[Math.floor(Math.random() * genre.length)];
 
-    //titlecase
-    firstPartTitleCase = firstPart.slice(0, 1).toUpperCase() + firstPart.slice(1);
-    secondPartTitleCase = secondPart.slice(0, 1).toUpperCase() + secondPart.slice(1);
-    suffixPartTitleCase = suffixPart.slice(0, 1).toUpperCase() + suffixPart.slice(1);
-    genrePartTitleCase = genrePart.slice(0, 1).toUpperCase() + genrePart.slice(1);
+  //titlecase
+  firstPartTitleCase = firstPart.slice(0, 1).toUpperCase() + firstPart.slice(1);
+  secondPartTitleCase = secondPart.slice(0, 1).toUpperCase() + secondPart.slice(1);
+  suffixPartTitleCase = suffixPart.slice(0, 1).toUpperCase() + suffixPart.slice(1);
+  genrePartTitleCase = genrePart.slice(0, 1).toUpperCase() + genrePart.slice(1);
 
 
 
-    //chance of adding layers
-    let chance = (Math.random());
-    if (chance >= 0.75) {
-        return `${firstPartTitleCase} ${secondPartTitleCase} ${suffixPartTitleCase}`
-    } else if (chance >= 0.5) {
-        return `${firstPartTitleCase} ${genrePartTitleCase} ${secondPartTitleCase}`
-    }
+  //chance of adding layers
+  let chance = (Math.random());
+  if (chance >= 0.75) {
+    return `${firstPartTitleCase} ${secondPartTitleCase} ${suffixPartTitleCase}`
+  } else if (chance >= 0.5) {
+    return `${firstPartTitleCase} ${genrePartTitleCase} ${secondPartTitleCase}`
+  }
 
-    else {
-        return `${firstPartTitleCase} ${secondPartTitleCase}`
-    }
+  else {
+    return `${firstPartTitleCase} ${secondPartTitleCase}`
+  }
 }
 
 function randomNumber() {
-    let num = Math.floor((Math.random() * 99));
-    if (num < 10) {
-        num = '0' + num;
-    } return num;
+  let num = Math.floor((Math.random() * 99));
+  if (num < 10) {
+    num = '0' + num;
+  } return num;
 }
+
+//all buttons
+const allButtons = document.querySelectorAll('.btn')
+
+const saveButton = document.getElementById('save');
 
 const randomButton = document.getElementById('randomize');
 
 function updatePatch(newName) {
-    const patchName = document.querySelector('.patchtext');
-    patchName.textContent = newName
+  const patchName = document.querySelector('.lcd__text');
+  patchName.textContent = newName
 }
 function updateNumber(newNumber) {
-    const patchNum = document.querySelector('.patchnum');
-    
-    patchNum.textContent = newNumber
+  const patchNum = document.querySelector('.lcd__digits');
+
+  patchNum.textContent = newNumber
 }
 
 
@@ -279,13 +284,61 @@ updateNumber(randomNumber());
 
 //give random patch on clicking button
 randomButton.addEventListener('click', function () {
-    randomButton.classList.add('playing');
-    updatePatch(randomPatch());
-    updateNumber(randomNumber());
+  randomButton.classList.add('playing');
+  updatePatch(randomPatch());
+  updateNumber(randomNumber());
 });
 
 function removeTransition(e) {
-    if (e.propertyName !== 'box-shadow') return;
-    e.target.classList.remove('playing');
+  if (e.propertyName !== 'box-shadow') return;
+  e.target.classList.remove('playing', 'pink');
+}
+
+// add click effect to all buttons, and remove transition effect
+allButtons.forEach(btn => {
+  btn.addEventListener('click', function() {
+    btn.classList.add('playing');
+  })
+  btn.addEventListener('transitionend', removeTransition);
+})
+
+// append current patch to Saved Patches list
+const patchName = document.querySelector('.lcd__text');
+const patchList = document.querySelector('.patch-bank__list');
+
+saveButton.addEventListener('click', function () {
+  saveButton.classList.add('playing', 'pink');
+  const li = document.createElement('li');
+  // prevent duplicate patches
+  if (patchName.textContent == patchList.lastChild.textContent) {
+    // do nothing
+  } else 
+  {
+    li.appendChild(document.createTextNode(patchName.textContent));
+    patchList.appendChild(li);
   }
-randomButton.addEventListener('transitionend', removeTransition);
+  // patchName.textContent
+});
+
+
+// clear patch bank list
+const clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', function () {
+  patchList.textContent = ' '
+})
+
+//copy to clipboard button
+const copyButton = document.getElementById('copy');
+const savedPatches = patchList.getElementsByTagName('li');
+
+copyButton.addEventListener('click', function() {
+  let copiedList = '';
+  
+  for (const patch of savedPatches) {
+    copiedList += `${patch.textContent}\n`
+  }
+  return navigator.clipboard.writeText(copiedList);
+  console.log(copiedList)
+})
+
